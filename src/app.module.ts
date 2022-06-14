6c3a00bd1df366b0d5d60c607dae797755b33f30
Ruwan Gunawardhana <17786634+Ruwanc@users.noreply.github.com>
2022-06-14 19:24:39 +0530

    add .env and connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({uri: config.get<string>('MONGO_URI'),useNewUrlParser:true})
+    }),
+    UserModule, 
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
